ab4f4999ca8f43b47d285268737951f0ea8d56b0
Tomy-my <azerouto@gmail.com>
2021-04-13 11:25:08 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+$titre = "Home"; 
+
+include("header.php"); 
+?>
+<style>
+body{        
+    background-image: url('./images/1.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+}
+
+</style>
+<div id="home">
+    <div class="home_titre">
+        <h2>Bienvenu au<br></h2>
+        <h1>Jura</h1>
+    </div>
+    <div class="descendre">
+        <a href="#descendre"> <i class="fas fa-arrow-down"></i> </a>
+    </div>
+</div>
+
+<div id="geo">
+    <div class="geo_left">
+
+    </div>
+    <div class="geo_right">
+
+    </div>
+</div>
