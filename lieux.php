2a102e53842c36c0cd450212e5ddc652fcb83c86
Tomy-my <azerouto@gmail.com>
2021-04-15 16:53:16 +0200

    add new page

---
+++ lieux.php
@@ -0,0 +1,44 @@
+<?php
+include("header.php");
+?>
+
+<div class="lieux_haut">
+    <div class="left_lieux">
+        <img src="images/lieux/abime.jpg">
+    </div>
+    <div class="right_lieux">
+        <img src="images/lieux/tufs.jpg">
+    </div>
+</div>
+<div class="lieux_center">
+    <div class="left_center">
+        <img src="images/lieux/herisson.jpg">
+    </div>
+    <div class="center_center">
+        <img src="images/lieux/boite.jpg">
+    </div>
+    <div class="right_center">
+        <img src="images/lieux/baume.jpg">
+    </div>
+</div>
+<div class="lieux_bas">
+    <div class="bas_1">
+        <img src="images/lieux/sel.jpg">
+    </div>
+    <div class="bas_2">
+        <img src="images/lieux/fromage.jpg">
+    </div>
+    <div class="bas_3">
+        <img src="images/lieux/lion.jpg">
+    </div>
+    <div class="bas_4">
+        <img src="images/lieux/notre-dame.jpg">
+    </div>
+    <div class="bas_5">
+        <img src="images/lieux/vieux.jpg">
+    </div>
+</div>
+
+<?php
+include("footer.php");
+?>
