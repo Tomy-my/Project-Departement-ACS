1f1be9016f971a34050cf32cd5a8a807c504a274
Tomy-my <azerouto@gmail.com>
2021-04-16 13:14:05 +0200

    Lieux Finish

--- lieux.php
+++ lieux.php
@@ -2,43 +2,96 @@
 include("header.php");
 ?>
 
-<div class="lieux_haut">
-    <div class="left_lieux">
-        <img src="images/lieux/abime.jpg">
-    </div>
-    <div class="right_lieux">
-        <img src="images/lieux/tufs.jpg">
-    </div>
-</div>
-<div class="lieux_center">
-    <div class="left_center">
-        <img src="images/lieux/herisson.jpg">
-    </div>
-    <div class="center_center">
-        <img src="images/lieux/boite.jpg">
-    </div>
-    <div class="right_center">
-        <img src="images/lieux/baume.jpg">
-    </div>
-</div>
-<div class="lieux_bas">
-    <div class="bas_1">
-        <img src="images/lieux/sel.jpg">
-    </div>
-    <div class="bas_2">
-        <img src="images/lieux/fromage.jpg">
-    </div>
-    <div class="bas_3">
-        <img src="images/lieux/lion.jpg">
-    </div>
-    <div class="bas_4">
-        <img src="images/lieux/notre-dame.jpg">
-    </div>
-    <div class="bas_5">
-        <img src="images/lieux/vieux.jpg">
-    </div>
-</div>
+<!-- ----------------------------------------- Le slide ----------------------------------------- -->
 
+<div class="slider">
+    <div class="slide current">
+        <div class="content">
+            <h1>Les Gorges de l'Abime</h1>
+            <p>
+                L'un des plus beaux sites du Jura.
+                <br>
+                Sentier de découverte, entièrement aménagé, permet de parcourir les étroites gorges dessinées par le torrent tumultueux de l'Abime 
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>La cascade des Tufs</h1>
+            <p>
+                Dans un paysage de reculée où l'eau s'infiltre dans un plateau, creuse une grotte et ressurgit en cascade, la cascade des tufs vous offre un spectacle à couper le souffle.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>Les cascades du Hérisson</h1>
+            <p>
+                En toutes saisons, les cascades du Hérisson offrent un spectacle sans cesse renouvelé, au rythme de la nature du Jura.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>La boite à montagne</h1>
+            <p>
+                Vivez des aventures en canyoning ou en via ferrata, découvrez des univers étonnants lors de randonnées en raquettes ou en ski nordique, parcourez monts & Vaux en VTT...
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>La réculée de Baume</h1>
+            <p>
+                Incontournable, surprenant, spectaculaire, les qualificatifs ne manquent pas pour décrire ce site exceptionnel dont la beauté n'a d'égal que sa fragilité.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>La Grande Saline - Musée du Sel</h1>
+            <p>
+                Haut lieu historique et industriel comtois, la Grande Saline de Salins a été reconnue patrimoine mondial de l’UNESCO en 2009.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>La fromagerie Thoiria</h1>
+            <p>
+                Site remarquable du goût. Située à Thoiria, dans la région de lacs, la Fruitière 1900 vous fera découvrir les secrets de la fabrication du Comté à la manière d'antan.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>Hôtel-Dieu de Lons-le-Saunier</h1>
+            <p>
+                L'hôtel-Dieu de Lons-le-Saunier est un ancien hôpital du XVIIIᵉ siècle de style classique de Lons-le-Saunier.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>Collégiale Notre-Dame</h1>
+            <p>
+                Notre-Dame de Dole est l'église principale de la ville de Dole.
+            </p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h1>Le vieux Dole</h1>
+            <p>
+                Ville contenant  l'églie collégiale de Notre-Dame.
+            </p>
+        </div>
+    </div>
+</div>
+    <div class="buttons">
+      <button id="prev"><i class="fas fa-arrow-left"></i></button>
+      <button id="next"><i class="fas fa-arrow-right"></i></button>
+    </div>
 <?php
 include("footer.php");
 ?>
